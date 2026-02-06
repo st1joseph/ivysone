e6dc78eff264a328ff21612381acc0165918b40f
st1joseph <124750277+st1joseph@users.noreply.github.com>
2026-02-07 00:59:45 +0530

    script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const messages = [
-    "Are you sure?",
-    "Really sure??",
+    "Are you sure Swadhaa?",
+    "Really sure Swadhaa??",
     "Are you positive?",
     "Pookie please...",
-    "Just think about it!",
+    "Just think about it, Swadhaa!",
     "If you say no, I will be really sad...",
     "I will be very sad...",
     "I will be very very very sad...",
